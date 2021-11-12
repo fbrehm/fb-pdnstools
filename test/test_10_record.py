b647f39fa8070c873545cca3747390764963474d
Frank Brehm <frank.brehm@pixelpark.com>
2021-11-12 16:09:51 +0100

    Adding test Response js files.

--- test/test_10_record.py
+++ test/test_10_record.py
@@ -14,11 +14,19 @@ import logging
 import logging.handlers
 import syslog
 import datetime
+import json
+
+from pathlib import Path
 
 try:
     import unittest2 as unittest
 except ImportError:
     import unittest
+
+import six
+
+import requests
+import requests_mock
 
 libdir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'lib'))
 sys.path.insert(0, libdir)
@@ -34,8 +42,117 @@ LOG = logging.getLogger('test_record')
 class TestPdnsRecord(FbPdnsToolsTestcase):
 
     # -------------------------------------------------------------------------
+    def setUp(self):
+
+        self.curdir = Path(os.path.dirname(os.path.abspath(__file__)))
+        self.zone_file = self.curdir / 'zone.js'
+        self.zone_rev_file = self.curdir / 'zone-rev.js'
+        self.zones_file = self.curdir / 'zones.js'
+        self.a_rrset_file = self.curdir / 'rrset-a.js'
+        self.a_rrset_file_comment = self.curdir / 'rrset-a-with-comment.js'
+        self.mx_rrset_file = self.curdir / 'rrset-mx.js'
+        self.soa_rrset_file = self.curdir / 'rrset-soa.js'
+
+        self.server_name = 'pdns-master.testing.net'
+        self.api_key = 'test123'
+
+        self.open_args = {}
+        if six.PY3:
+            self.open_args['encoding'] = 'utf-8'
+            self.open_args['errors'] = 'surrogateescape'
+
+        self.server_version = '4.1.10-mocked'
+
+        self.server_list_data = [
+            {
+                "config_url": "/api/v1/servers/localhost/config{/config_setting}",
+                "daemon_type": "authoritative",
+                "id": "localhost",
+                "type": "Server",
+                "url": "/api/v1/servers/localhost",
+                "version": "{}".format(self.server_version),
+                "zones_url": "/api/v1/servers/localhost/zones{/zone}"
+            }
+        ]
+
+    # -------------------------------------------------------------------------
+    def tearDown(self):
+
+        pass
+
+    # -------------------------------------------------------------------------
+    def get_js_a_rrset(self):
+
+        ret = None
+        with self.a_rrset_file.open('r', **self.open_args) as fh:
+            ret = json.load(fh)
+        return ret
+
+    # -------------------------------------------------------------------------
+    def get_js_a_rrset_comment(self):
+
+        ret = None
+        with self.a_rrset_file_comment.open('r', **self.open_args) as fh:
+            ret = json.load(fh)
+        return ret
+
+    # -------------------------------------------------------------------------
+    def get_js_mx_rrset(self):
+
+        ret = None
+        with self.mx_rrset_file.open('r', **self.open_args) as fh:
+            ret = json.load(fh)
+        return ret
+
+    # -------------------------------------------------------------------------
+    def get_js_soa_rrset(self):
+
+        ret = None
+        with self.soa_rrset_file.open('r', **self.open_args) as fh:
+            ret = json.load(fh)
+        return ret
+
+    # -------------------------------------------------------------------------
+    def get_js_zone(self):
+
+        ret = None
+        with self.zone_file.open('r', **self.open_args) as fh:
+            ret = json.load(fh)
+        return ret
+
+    # -------------------------------------------------------------------------
+    def get_js_zone_rev(self):
+
+        ret = None
+        with self.zone_rev_file.open('r', **self.open_args) as fh:
+            ret = json.load(fh)
+        return ret
+
+    # -------------------------------------------------------------------------
+    def get_js_zones(self):
+
+        ret = None
+        with self.zones_file.open('r', **self.open_args) as fh:
+            ret = json.load(fh)
+        return ret
+
+    # -------------------------------------------------------------------------
+    def get_js_serverlist(self, index=None):
+
+        import json
+
+        if index is None:
+            data = self.server_list_data
+        else:
+            data = self.server_list_data[index]
+
+        return json.dumps(data)
+
+    # -------------------------------------------------------------------------
     def test_import_modules(self):
 
+        if self.verbose:
+            print()
         LOG.info("Test importing record module ...")
 
         LOG.debug("Importing fb_pdnstools.record ...")
@@ -52,6 +169,8 @@ class TestPdnsRecord(FbPdnsToolsTestcase):
     # -------------------------------------------------------------------------
     def test_pdns_record(self):
 
+        if self.verbose:
+            print()
         LOG.info("Testing class PowerDNSRecord ...")
 
         test_content = "www.testing.com."
@@ -94,6 +213,8 @@ class TestPdnsRecord(FbPdnsToolsTestcase):
     # -------------------------------------------------------------------------
     def test_pdns_record_equality(self):
 
+        if self.verbose:
+            print()
         LOG.info("Testing equal operator of class PowerDNSRecord ...")
 
         test_content = "www.testing.com."
@@ -130,6 +251,8 @@ class TestPdnsRecord(FbPdnsToolsTestcase):
     # -------------------------------------------------------------------------
     def test_pdns_record_gt(self):
 
+        if self.verbose:
+            print()
         LOG.info("Testing the greater than operator of class PowerDNSRecord ...")
 
         test_content = "www.1testing.com."
@@ -184,6 +307,8 @@ class TestPdnsRecord(FbPdnsToolsTestcase):
     # -------------------------------------------------------------------------
     def test_pdns_record_lt(self):
 
+        if self.verbose:
+            print()
         LOG.info("Testing the less than operator of class PowerDNSRecord ...")
 
         test_content = "www.1testing.com."
@@ -238,6 +363,8 @@ class TestPdnsRecord(FbPdnsToolsTestcase):
     # -------------------------------------------------------------------------
     def test_pdns_recordset_comment(self):
 
+        if self.verbose:
+            print()
         LOG.info("Testing class PowerDNSRecordSetComment ...")
 
         test_account = 'tester'
@@ -308,6 +435,26 @@ class TestPdnsRecord(FbPdnsToolsTestcase):
         e = cm.exception
         LOG.debug("{} raised: {}".format(e.__class__.__name__, e))
 
+    # -------------------------------------------------------------------------
+    def test_pdns_recordset_simple(self):
+
+        if self.verbose:
+            print()
+        LOG.info("Testing class PowerDNSRecordSet ...")
+
+        from fb_pdnstools.record import PowerDNSRecordSet
+
+        js_rrset = self.get_js_a_rrset()
+
+        rrset = PowerDNSRecordSet.init_from_dict(
+            js_rrset, appname=self.appname, verbose=self.verbose)
+        LOG.debug("RecordSet: %%r: {!r}".format(rrset))
+        if self.verbose > 1:
+            LOG.debug("RecordSet: %%s: {}".format(rrset))
+            LOG.debug("rrset.as_dict():\n{}".format(pp(rrset.as_dict())))
+        LOG.debug("RecordSet.as_dict(minimal=True): {}".format(
+            pp(rrset.as_dict(minimal=True))))
+
 
 # =============================================================================
 if __name__ == '__main__':
@@ -328,6 +475,7 @@ if __name__ == '__main__':
     suite.addTest(TestPdnsRecord('test_pdns_record_gt', verbose))
     suite.addTest(TestPdnsRecord('test_pdns_record_lt', verbose))
     suite.addTest(TestPdnsRecord('test_pdns_recordset_comment', verbose))
+    suite.addTest(TestPdnsRecord('test_pdns_recordset_simple', verbose))
 
     runner = unittest.TextTestRunner(verbosity=verbose)
 
