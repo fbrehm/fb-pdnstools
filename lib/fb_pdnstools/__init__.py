34cb6b4ed87b90c2da5d27e79135c52047323651
Frank Brehm <frank.brehm@pixelpark.com>
2021-12-07 10:20:28 +0100

    Version bump to 0.4.4 and dch

--- lib/fb_pdnstools/__init__.py
+++ lib/fb_pdnstools/__init__.py
@@ -41,7 +41,7 @@ from .xlate import XLATOR
 from .errors import PowerDNSHandlerError, PDNSApiError, PDNSApiNotAuthorizedError
 from .errors import PDNSApiNotFoundError, PDNSApiValidationError, PDNSApiRateLimitExceededError
 
-__version__ = '0.4.3'
+__version__ = '0.4.4'
 LOG = logging.getLogger(__name__)
 LIBRARY_NAME = "fb-pdns-api-client"
 
