fa04435ce764f941f1845fef3655528fa16e235d
Frank Brehm <frank@brehm-online.com>
2023-07-20 11:15:16 +0200

    Applying flake8 rules to test/test_00_fb_pdnstools.py

--- test/test_00_fb_pdnstools.py
+++ test/test_00_fb_pdnstools.py
@@ -1,18 +1,18 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-'''
+"""
+@summary: Test script (and module) for unit tests on fb-pdnstools base classes.
+
 @author: Frank Brehm
 @contact: frank@brehm-online.com
 @copyright: © 2023 Frank Brehm, Berlin
 @license: LGPL3
-@summary: test script (and module) for unit tests on fb-pdnstools base classes
-'''
+"""
 
-import os
-import sys
 import logging
 import logging.handlers
-
+import os
+import sys
 try:
     import unittest2 as unittest
 except ImportError:
@@ -21,8 +21,10 @@ except ImportError:
 libdir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'lib'))
 sys.path.insert(0, libdir)
 
-from fb_tools.common import pp
+# Third party modules
+# from fb_tools.common import pp
 
+# Own modules
 from general import FbPdnsToolsTestcase, get_arg_verbose, init_root_logger
 
 LOG = logging.getLogger('test_fb_pdnstools')
@@ -30,65 +32,66 @@ LOG = logging.getLogger('test_fb_pdnstools')
 
 # =============================================================================
 class TestPdnsToolsBase(FbPdnsToolsTestcase):
+    """Testcase for tests on fb_pdnstools/__init__.py and fb_pdnstools.errors."""
 
     # -------------------------------------------------------------------------
     def test_import_modules(self):
+        """Testing import of modules fb_pdnstools/__init__.py and fb_pdnstools.errors ..."""
+        LOG.info('Test importing main module ...')
 
-        LOG.info("Test importing main module ...")
-
-        LOG.debug("Importing fb_pdnstools ...")
+        LOG.debug('Importing fb_pdnstools ...')
         import fb_pdnstools
 
-        LOG.debug("Version of fb_pdnstools: {!r}.".format(fb_pdnstools.__version__))
+        LOG.debug('Version of fb_pdnstools: {!r}.'.format(fb_pdnstools.__version__))
 
-        LOG.debug("Importing fb_pdnstools.errors ...")
+        LOG.debug('Importing fb_pdnstools.errors ...')
         import fb_pdnstools.errors
-        LOG.debug("Version of fb_pdnstools.errors: {!r}.".format(
+        LOG.debug('Version of fb_pdnstools.errors: {!r}.'.format(
             fb_pdnstools.errors.__version__))
 
     # -------------------------------------------------------------------------
     def test_error_classes(self):
+        """Test instantiating of different exception classes in fb_pdnstools.errors."""
+        LOG.info('Testing loading and function of different error handler classes ...')
 
-        LOG.info("Testing loading and function of different error handler classes ...")
-
-        LOG.debug("Testing PowerDNSHandlerError from fb_pdnstools.errors ...")
+        LOG.debug('Testing PowerDNSHandlerError from fb_pdnstools.errors ...')
         from fb_pdnstools.errors import PowerDNSHandlerError
         with self.assertRaises(PowerDNSHandlerError) as cm:
             raise PowerDNSHandlerError('bla')
         e = cm.exception
-        LOG.debug("Got a {c}: {e}".format(c=e.__class__.__name__, e=e))
+        LOG.debug('Got a {c}: {e}'.format(c=e.__class__.__name__, e=e))
 
-        LOG.debug("Testing PowerDNSZoneError from fb_pdnstools.errors ...")
+        LOG.debug('Testing PowerDNSZoneError from fb_pdnstools.errors ...')
         from fb_pdnstools.errors import PowerDNSZoneError
         with self.assertRaises(PowerDNSZoneError) as cm:
             raise PowerDNSZoneError('bla')
         e = cm.exception
-        LOG.debug("Got a {c}: {e}".format(c=e.__class__.__name__, e=e))
+        LOG.debug('Got a {c}: {e}'.format(c=e.__class__.__name__, e=e))
 
-        LOG.debug("Testing PowerDNSRecordError from fb_pdnstools.errors ...")
+        LOG.debug('Testing PowerDNSRecordError from fb_pdnstools.errors ...')
         from fb_pdnstools.errors import PowerDNSRecordError
         with self.assertRaises(PowerDNSRecordError) as cm:
             raise PowerDNSRecordError('bla')
         e = cm.exception
-        LOG.debug("Got a {c}: {e}".format(c=e.__class__.__name__, e=e))
+        LOG.debug('Got a {c}: {e}'.format(c=e.__class__.__name__, e=e))
 
-        LOG.debug("Testing PowerDNSRecordSetError from fb_pdnstools.errors ...")
+        LOG.debug('Testing PowerDNSRecordSetError from fb_pdnstools.errors ...')
         from fb_pdnstools.errors import PowerDNSRecordSetError
         with self.assertRaises(PowerDNSRecordSetError) as cm:
             raise PowerDNSRecordSetError('bla')
         e = cm.exception
-        LOG.debug("Got a {c}: {e}".format(c=e.__class__.__name__, e=e))
+        LOG.debug('Got a {c}: {e}'.format(c=e.__class__.__name__, e=e))
 
-        LOG.debug("Testing PowerDNSWrongSoaDataError from fb_pdnstools.errors ...")
+        LOG.debug('Testing PowerDNSWrongSoaDataError from fb_pdnstools.errors ...')
         from fb_pdnstools.errors import PowerDNSWrongSoaDataError
         with self.assertRaises(PowerDNSWrongSoaDataError) as cm:
             raise PowerDNSWrongSoaDataError({'uhu': 'banane'})
         e = cm.exception
-        LOG.debug("Got a {c}: {e}".format(c=e.__class__.__name__, e=e))
+        LOG.debug('Got a {c}: {e}'.format(c=e.__class__.__name__, e=e))
 
-        msg = "Undfined test error"
+        msg = 'Undfined test error'
 
-        LOG.debug("Testing PDNSApiError from fb_pdnstools.errors ...")
+        LOG.debug('Testing PDNSApiError from fb_pdnstools.errors ...')
         from fb_pdnstools.errors import PDNSApiError
         uris = (None, 'https://pdnsmaster.uhu-banane.eu')
         code = 404
@@ -96,42 +99,42 @@ class TestPdnsToolsBase(FbPdnsToolsTestcase):
             with self.assertRaises(PDNSApiError) as cm:
                 raise PDNSApiError(code, msg, uri)
             e = cm.exception
-            LOG.debug("Got a {c}: {e}".format(c=e.__class__.__name__, e=e))
+            LOG.debug('Got a {c}: {e}'.format(c=e.__class__.__name__, e=e))
 
-        LOG.debug("Testing PDNSApiNotAuthorizedError from fb_pdnstools.errors ...")
+        LOG.debug('Testing PDNSApiNotAuthorizedError from fb_pdnstools.errors ...')
         from fb_pdnstools.errors import PDNSApiNotAuthorizedError
         with self.assertRaises(PDNSApiNotAuthorizedError) as cm:
             raise PDNSApiNotAuthorizedError(401, msg)
         e = cm.exception
-        LOG.debug("Got a {c}: {e}".format(c=e.__class__.__name__, e=e))
+        LOG.debug('Got a {c}: {e}'.format(c=e.__class__.__name__, e=e))
 
-        LOG.debug("Testing PDNSApiNotFoundError from fb_pdnstools.errors ...")
+        LOG.debug('Testing PDNSApiNotFoundError from fb_pdnstools.errors ...')
         from fb_pdnstools.errors import PDNSApiNotFoundError
         with self.assertRaises(PDNSApiNotFoundError) as cm:
             raise PDNSApiNotFoundError(404, msg)
         e = cm.exception
-        LOG.debug("Got a {c}: {e}".format(c=e.__class__.__name__, e=e))
+        LOG.debug('Got a {c}: {e}'.format(c=e.__class__.__name__, e=e))
 
-        LOG.debug("Testing PDNSApiValidationError from fb_pdnstools.errors ...")
+        LOG.debug('Testing PDNSApiValidationError from fb_pdnstools.errors ...')
         from fb_pdnstools.errors import PDNSApiValidationError
         with self.assertRaises(PDNSApiValidationError) as cm:
             raise PDNSApiValidationError(422, msg, 'https://pdnsmaster.uhu-banane.eu')
         e = cm.exception
-        LOG.debug("Got a {c}: {e}".format(c=e.__class__.__name__, e=e))
+        LOG.debug('Got a {c}: {e}'.format(c=e.__class__.__name__, e=e))
 
-        LOG.debug("Testing PDNSApiRateLimitExceededError from fb_pdnstools.errors ...")
+        LOG.debug('Testing PDNSApiRateLimitExceededError from fb_pdnstools.errors ...')
         from fb_pdnstools.errors import PDNSApiRateLimitExceededError
         with self.assertRaises(PDNSApiRateLimitExceededError) as cm:
             raise PDNSApiRateLimitExceededError(429, msg)
         e = cm.exception
-        LOG.debug("Got a {c}: {e}".format(c=e.__class__.__name__, e=e))
+        LOG.debug('Got a {c}: {e}'.format(c=e.__class__.__name__, e=e))
 
-        LOG.debug("Testing PDNSApiRequestError from fb_pdnstools.errors ...")
+        LOG.debug('Testing PDNSApiRequestError from fb_pdnstools.errors ...')
         from fb_pdnstools.errors import PDNSApiRequestError
         with self.assertRaises(PDNSApiRequestError) as cm:
             raise PDNSApiRequestError(419, 'I am not a tea pot.')
         e = cm.exception
-        LOG.debug("Got a {c}: {e}".format(c=e.__class__.__name__, e=e))
+        LOG.debug('Got a {c}: {e}'.format(c=e.__class__.__name__, e=e))
 
 
 # =============================================================================
@@ -142,7 +145,7 @@ if __name__ == '__main__':
         verbose = 0
     init_root_logger(verbose)
 
-    LOG.info("Starting tests ...")
+    LOG.info('Starting tests ...')
 
     loader = unittest.TestLoader()
     suite = unittest.TestSuite()
