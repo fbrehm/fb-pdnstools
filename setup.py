eaf5ee80b548df66a1e1235ceee3b804ca51175c
Frank Brehm <frank@brehm-online.com>
2024-02-04 12:47:52 +0100

    Version bump to 0.6.2 and dch

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@
 @author: Frank Brehm
 @contact: frank@brehm-online.com
 @license: LGPL3+
-@copyright: © 2023 Frank Brehm, Berlin
+@copyright: © 2024 Frank Brehm, Berlin
 """
 from __future__ import print_function
 
@@ -63,7 +63,7 @@ __debian_pkg_name__ = 'fb-pdnstools'
 
 __author__ = 'Frank Brehm'
 __contact__ = 'frank@brehm-online.com'
-__copyright__ = '(C) 2023 Frank Brehm, Berlin'
+__copyright__ = '(C) 2024 Frank Brehm, Berlin'
 __license__ = 'LGPL3+'
 __url__ = 'https://github.com/fbrehm/fb-pdnstools'
 
